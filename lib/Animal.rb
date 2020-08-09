3a84664e79fad868e4a0c77cfb5f869b6671f0fc
sbalgobin94 <sbalgobin94@gmail.com>
2020-08-09 17:16:40 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,22 @@
 class Animal
+    attr_reader :species, :nickname
+    attr_accessor :weight, :zoo
+
+    @@all = []
+
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        self.all.select {|animals| animals.species == species}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -2,9 +2,16 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
+central_park = Zoo.new("Central Park", "Manhattan")
+lincoln_park = Zoo.new("Lincoln Park", "Queens")
 
-#Test your code here
+cat = Animal.new("cat", "52", "Tommy")
+cat.zoo = central_park
 
+dog = Animal.new("cat", "70", "Ruby")
+dog.zoo = central_park
 
+find = Animal.find_by_species("cat")
+species = central_park.animal_species
 binding.pry
 puts "done"
