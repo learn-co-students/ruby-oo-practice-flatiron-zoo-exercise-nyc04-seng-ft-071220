25b93420dfe6711bcb2b0fc0f6ca4812eaa5cb6b
Bella Evalle <bellaevalle@yahoo.com>
2020-08-09 21:40:32 +0000

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,18 @@
 class Animal
 
+attr_accessor :weight, :zoo
+attr_reader :species, :nick_name
+
+@@all = []
+
+def initialize (species, weight, nick_name)
+  @species = species
+  @weight = weight
+  @nick_name = nick_name
+  @zoo = nil
+  @all << self
+end
+
+def self.all
+  @@all
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,31 @@
 class Zoo
 
+  attr_accessor :name, :loation
+
+  @@all = []
+
+  def initialize (name, location)
+     @name = name
+     @location = location
+     @@all << self
+  end
+
+  def self.all
+     @@all
+  end
+  
+  def add_animal(animal)
+    animal.zoo = self
+  end
+  
+  def animal_species
+    selected_array = Animal.all.select do |animal|
+      animal.zoo == self
+    end
+    mapped_array = selected_array.map do |animal|
+      animal.species 
+    end
+    mapped_array.uniq 
+  end 
+  
 end
