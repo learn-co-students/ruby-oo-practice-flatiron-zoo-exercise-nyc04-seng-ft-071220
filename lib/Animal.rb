cdddcd38f56cb9c7259bf11ea23c93bd2bce7f31
Elronia <zar.catherine@gmail.com>
2020-08-09 22:29:52 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Animal
+    attr_accessor :weight, :zoo
+    attr_reader :species, :nickname
+
+    @@all = []
+
+    def initialize(sself.allpecies, weight, nick_name)
+        @species = species
+        @weight = weight
+        @nickname = nick_name
+        @zoo = nil
+        @@all << self
+    end
+
+    # def inspect
+    #     "#{self.class}: @species = #{species}"
+    # end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species) #returns an array of all the animals, which are of that species
+        self.all.select do |animal|
+            # binding.pry
+            animal.species == species
+        end
+    end
 
 end
